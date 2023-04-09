d7ccbedeef77f87470d062a49268041739c28daf
Matt Brouwer <notjonbonjovi@gmail.com>
2023-04-09 13:05:27 -0400

    callbacks

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function receivesAFunction(callback) {
+    return callback();
+}
+function returnsANamedFunction() {
+    return function receivesAFunction(name) {
+        console.log(name);
+    }
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {}
+
+}
